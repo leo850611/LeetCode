37379bb39c39f808f3b354a9d12d3d61311f62e0
Leo <leo850611@gmail.com>
2022-12-14 00:33:35 +0800

    implement Problem001

--- LeetCode/Problems/001. Two Sum/Problem001.cs
+++ LeetCode/Problems/001. Two Sum/Problem001.cs
@@ -1,9 +1,21 @@
+using System;
+
 namespace Problems._001._Two_Sum
 {
     public class Problem001
     {
         public int[] TwoSum(int[] nums, int target)
         {
+            foreach (var num in nums)
+            {
+                foreach (var i in nums)
+                {
+                    if (num + i == target)
+                    {
+                        return new int[] { Array.IndexOf(nums, num), Array.IndexOf(nums, i) };
+                    }
+                }
+            }
             return new int[]{};
         }
     }
