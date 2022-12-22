124f86e50a628f9f30de0dbb6970f8682d71b758
Leo <leo850611@gmail.com>
2022-12-23 02:49:54 +0800

    try implement Problem021

--- LeetCode/Problems/021. Merge Two Sorted Lists/Problem021.cs
+++ LeetCode/Problems/021. Merge Two Sorted Lists/Problem021.cs
@@ -4,7 +4,39 @@ namespace Problems._021._Merge_Two_Sorted_Lists
     {
         public ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
-            return null;
+            var list1LastNode = list1;
+            var list2LastNode = list2;
+            while (list1LastNode != null)
+            {
+                if (list2LastNode != null && list1LastNode.val <= list2LastNode.val)
+                {
+                    var temp1 = list1LastNode.next;
+                    var temp2 = list2LastNode.next;
+
+                    list2LastNode.next = list1LastNode.next;
+                    list1LastNode.next = list2LastNode;
+
+
+                    if (temp1 == null)
+                    {
+                        list1LastNode.next = list2LastNode;
+                        list2LastNode = null;
+                    }
+                    else
+                    {
+                        list1LastNode = temp1;
+                        list2LastNode = temp2;
+                    }
+                }
+                else if (list2LastNode == null)
+                {
+                    list1LastNode = list1LastNode.next;
+                }
+            }
+
+
+            
+            return list1;
         }
     }
 
--- LeetCode/ProblemsTests/Problem021Tests.cs
+++ LeetCode/ProblemsTests/Problem021Tests.cs
@@ -16,34 +16,44 @@ public class Problem021Tests
     }
 
     [Test]
-    public void example_1()
+    public void example_a()
     {
-        var inputList1 = GenerateNode(new List<int>() { 1, 2, 4 });
-        var inputList2 = GenerateNode(new List<int>() { 1, 3, 4 });
+        var inputList1 = GenerateNode(new List<int> { 1, 2 });
+        var inputList2 = GenerateNode(new List<int> { 1, 3 });
         var node = new Problem021().MergeTwoLists(inputList1, inputList2);
 
         var resultList = GetResultList(node);
 
-        Assert.That(resultList, Is.EqualTo(new List<int>() { 1, 1, 2, 3, 4, 4 }));
+        Assert.That(resultList, Is.EqualTo(new List<int> { 1, 1, 2, 3 }));
     }
+
+    [Test]
+    public void example_b()
+    {
+        var inputList1 = GenerateNode(new List<int> { 1 });
+        var inputList2 = GenerateNode(new List<int> { 1, 3 });
+        var node = new Problem021().MergeTwoLists(inputList1, inputList2);
+
+        var resultList = GetResultList(node);
+
+        Assert.That(resultList, Is.EqualTo(new List<int> { 1, 1, 3 }));
+    }
+
 
     private ListNode? GenerateNode(List<int> ints)
     {
-        if (ints.Count == 0)
-        {
-            return null;
-        }
+        if (ints.Count == 0) return null;
 
         var node = new ListNode();
         var lastNode = node;
 
         foreach (var i in ints)
         {
-            lastNode.next = new ListNode(i, null);
+            lastNode.next = new ListNode(i);
             lastNode = lastNode.next;
         }
 
-        return node;
+        return node.next;
     }
 
     private List<int> GetResultList(ListNode node)
