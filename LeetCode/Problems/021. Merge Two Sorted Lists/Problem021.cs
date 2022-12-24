5d986051ddeaad3d67fd8fc7e76ea9c349554000
Leo <leo850611@hotmail.com>
2022-12-24 17:48:27 +0800

    add MergeTwoLists_recursive

--- LeetCode/Problems/021. Merge Two Sorted Lists/Problem021.cs
+++ LeetCode/Problems/021. Merge Two Sorted Lists/Problem021.cs
@@ -2,6 +2,25 @@ namespace Problems._021._Merge_Two_Sorted_Lists
 {
     public class Problem021
     {
+        public ListNode MergeTwoLists_recursive(ListNode list1, ListNode list2)
+        {
+            if (list1 == null || list2 == null)
+            {
+                return list1 ?? list2;
+            }
+            
+            if (list1.val < list2.val)
+            {
+                list1.next = MergeTwoLists_recursive(list1.next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.next = MergeTwoLists_recursive(list1, list2.next);
+                return list2;
+            }
+        }
+        
         public ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
             var listNode = new ListNode();
