5402ce787203dc7340a7122b4b9844b882c0afb5
Leo <leo850611@hotmail.com>
2023-01-01 21:31:01 +0800

    Problems121 pass

--- LeetCode/Problems/121. Best Time to Buy and Sell Stock/Problem121.cs
+++ LeetCode/Problems/121. Best Time to Buy and Sell Stock/Problem121.cs
@@ -1,7 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Problems._121._Best_Time_to_Buy_and_Sell_Stock
 {
     public class Problem121
     {
-        
+        public int MaxProfit(int[] prices)
+        {
+            var profitList = new List<int> {0};
+            for (var i = 0; i < prices.Length - 1; i++)
+            {
+                var currentProfit = prices[i + 1] - prices[i];
+                var sumProfit = profitList[i] + currentProfit;
+                var maxProfit = Math.Max(currentProfit, sumProfit);
+                profitList.Add(maxProfit);
+            }
+
+            return profitList.Max();
+        }
     }
 }
--- LeetCode/ProblemsTests/Problem121Tests.cs
+++ LeetCode/ProblemsTests/Problem121Tests.cs
@@ -1,7 +1,15 @@
+using Problems._121._Best_Time_to_Buy_and_Sell_Stock;
+
 namespace ProblemsTests;
 
 [TestFixture]
 public class Problem121Tests
 {
-    
+    [TestCase(new int[] {7, 1, 5, 3, 6, 4}, 5)]
+    [TestCase(new int[] {7, 6, 4, 3, 1}, 0)]
+    public void example_1(int[] prices, int expected)
+    {
+        var actual = new Problem121().MaxProfit(prices);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
