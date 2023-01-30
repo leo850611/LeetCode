cbd97a2d3c798a1abda74f34132e49117af2be82
Leo <leo850611@gmail.com>
2023-01-30 23:18:27 +0800

    Problem206 pass

--- LeetCode/Problems/206. Reverse Linked List/Problem206.cs
+++ LeetCode/Problems/206. Reverse Linked List/Problem206.cs
@@ -1,8 +1,28 @@
+using Problems.Models;
+
 namespace Problems._206._Reverse_Linked_List
 {
     // https://leetcode.com/problems/reverse-linked-list/description/
     public class Problem206
     {
-        
+        private ListNode _temp = null;
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+                
+            var headNext = head.next;
+            head.next = _temp;
+            _temp = head;
+                
+            if (headNext == null)
+            {
+                return head;
+            }
+                
+            return ReverseList(headNext);
+        }
     }
 }
--- LeetCode/ProblemsTests/Problem206Tests.cs
+++ LeetCode/ProblemsTests/Problem206Tests.cs
@@ -1,7 +1,46 @@
+using Problems._206._Reverse_Linked_List;
+using Problems.Models;
+
 namespace ProblemsTests;
 
 [TestFixture]
 public class Problem206Tests
 {
-    
+    [Test]
+    public void example_1()
+    {
+        var inputList = GenerateNode(new List<int> { 1, 2, 3, 4, 5 });
+        var actual = new Problem206().ReverseList(inputList);
+        var resultList = GetResultList(actual);
+        Assert.That(resultList, Is.EqualTo(new List<int> { 5, 4, 3, 2, 1 }));
+    }
+
+    private ListNode? GenerateNode(List<int> ints)
+    {
+        if (ints.Count == 0) return null;
+
+        var node = new ListNode();
+        var lastNode = node;
+
+        foreach (var i in ints)
+        {
+            lastNode.next = new ListNode(i);
+            lastNode = lastNode.next;
+        }
+
+        return node.next;
+    }
+
+    private List<int> GetResultList(ListNode node)
+    {
+        var resultList = new List<int>();
+        while (node != null)
+        {
+            var value = node.val;
+            resultList.Add(value);
+            node = node.next;
+        }
+
+        return resultList;
+    }
 }
