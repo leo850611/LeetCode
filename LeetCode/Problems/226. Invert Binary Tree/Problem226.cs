d4cddaa5fa588adf9f32200804e65fc7044bed21
Leo <leo850611@hotmail.com>
2023-02-06 19:42:22 +0800

    Problem226 use while loop

--- LeetCode/Problems/226. Invert Binary Tree/Problem226.cs
+++ LeetCode/Problems/226. Invert Binary Tree/Problem226.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Problems.Models;
 
 namespace Problems._226._Invert_Binary_Tree
@@ -18,6 +19,37 @@ namespace Problems._226._Invert_Binary_Tree
 
             InvertTree(root.right);
             InvertTree(root.left);
+
+            return root;
+        }
+        
+        public TreeNode InvertTree_while(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+
+            if (root != null)
+            {
+                queue.Enqueue(root);
+            }
+
+            while (queue.Count != 0)
+            {
+                var treeNode = queue.Dequeue();
+                
+                var rootLeft = treeNode.left;
+                treeNode.left = treeNode.right;
+                treeNode.right = rootLeft;
+
+                if (treeNode.left != null)
+                {
+                    queue.Enqueue(treeNode.left);
+                }
+
+                if (treeNode.right != null)
+                {
+                    queue.Enqueue(treeNode.right);
+                }
+            }
 
             return root;
         }
