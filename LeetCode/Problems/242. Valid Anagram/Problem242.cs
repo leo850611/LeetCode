cfcb483591dc2da4e002fa28b9d8f94bc87bd06b
Leo <leo850611@gmail.com>
2023-02-20 00:54:14 +0800

    init Problem242

---
+++ LeetCode/Problems/242. Valid Anagram/Problem242.cs
@@ -0,0 +1,11 @@
+namespace Problems._242._Valid_Anagram
+{
+    public class Problem242
+    {
+        // https://leetcode.com/problems/valid-anagram/
+        public bool IsAnagram(string s, string t)
+        {
+            return false;
+        }
+    }
+}
